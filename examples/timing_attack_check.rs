3729b926ca037a3e0603d09ed365e2c35f264601
Nilirad <gisquerin@gmail.com>
2026-06-09 16:03:19 +0200

    Optimize DudeCT benchmark

--- examples/timing_attack_check.rs
+++ examples/timing_attack_check.rs
@@ -1,23 +1,14 @@
 use dudect_bencher::{BenchRng, Class, CtRunner, ctbench_main};
 
 use dudect_bencher::rand::RngExt;
-use subtle::ConstantTimeEq;
-
-#[inline(never)]
-fn verify_api_key(expected: Option<&String>, provided: Option<&str>) -> bool {
-    expected.zip(provided).is_some_and(|(key, header)| {
-        let key_bytes = key.as_bytes();
-        let header_bytes = header.as_bytes();
-        key_bytes.ct_eq(&header_bytes).into()
-    })
-}
+use rand::distr::{Alphanumeric, SampleString};
+use relay::domain::NonEmptyString;
+use relay::verify_api_key;
 
 fn bench_verify_api_key(runner: &mut CtRunner, rng: &mut BenchRng) {
     const SET_SIZE: usize = 100_000;
 
-    let expected = String::from("super_secret_api_key_12345");
-    let correct_input = "super_secret_api_key_12345";
-    let incorrect_input = "Zuper_secret_api_key_12345";
+    let expected = NonEmptyString::new(String::from("super_secret_api_key_12345")).unwrap();
 
     let mut classes = Vec::with_capacity(SET_SIZE);
     let mut inputs = Vec::with_capacity(SET_SIZE);
@@ -25,17 +16,16 @@ fn bench_verify_api_key(runner: &mut CtRunner, rng: &mut BenchRng) {
     for _ in 0..SET_SIZE {
         if rng.random::<bool>() {
             classes.push(Class::Right);
-            inputs.push(correct_input);
+            inputs.push(String::from("super_secret_api_key_12345"));
         } else {
             classes.push(Class::Left);
-            inputs.push(incorrect_input);
+            let random_string = Alphanumeric.sample_string(rng, expected.len());
+            inputs.push(random_string);
         }
     }
 
-    for (class, input) in classes.into_iter().zip(inputs.into_iter()) {
-        runner.run_one(class, || {
-            verify_api_key(Some(&expected), Some(input));
-        });
+    for (class, input) in classes.into_iter().zip(inputs) {
+        runner.run_one(class, || verify_api_key(Some(&expected), Some(&input)));
     }
 }
 
